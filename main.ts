327915550e0fc254a0f67bcfc77e799a552b7cf5
pigplouf <103496137+pigplouf@users.noreply.github.com>
2022-04-12 11:26:48 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
         . . . . .
         . . . . .
         . . . . .
@@ -7,13 +16,15 @@ input.onButtonPressed(Button.A, function () {
         . . . . .
         `)
 })
-basic.showLeds(`
-    . # . # .
-    # # # # #
-    # # # # #
-    . # # # .
-    . . # . .
-    `)
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
 basic.forever(function () {
 	
 })
